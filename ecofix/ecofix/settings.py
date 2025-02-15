3d1e315d848a253ceb15f75f20cd27b6bb2bfe7e
im-AyanRoy <ayanroysince2003official@gmail.com>
2025-02-16 01:58:27 +0530

    settings

--- ecofix/ecofix/settings.py
+++ ecofix/ecofix/settings.py
@@ -13,6 +13,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
